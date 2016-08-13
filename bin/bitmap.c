war2/war2edit

commit 75c94b0800a656f956d94bf47926872aba8c520a

bitmap: fix invalid cursor after an out-of-bounds

bin/bitmap.c
@@ -406,8 +406,7 @@ _mouse_move_cb(void        *data,
      }
    else
      {
-        if (ed->sel_unit != PUD_UNIT_NONE)
-          bitmap_cursor_state_evaluate(ed, cx, cy);
+        bitmap_cursor_state_evaluate(ed, cx, cy);
      }
 
    if (bitmap_cursor_enabled_get(ed))

commit 39c6c54dcec0f0f9364af04d1cfaa95dfaa6a311

bitmap: better fix of cursor state after out of bounds
Revert 75c94b0800a656f956d94bf47926872aba8c520a which
introduced a regression, and fix the original problem
better.

bin/bitmap.c
@@ -403,10 +403,17 @@ _mouse_move_cb(void        *data,
        (ed->bitmap.cy + ed->bitmap.ch > (int)ed->pud->map_h))
      {
         bitmap_cursor_enabled_set(ed, EINA_FALSE);
+        ed->was_oob = EINA_TRUE;
      }
    else
      {
-        bitmap_cursor_state_evaluate(ed, cx, cy);
+        if (ed->was_oob)
+          {
+             bitmap_cursor_enabled_set(ed, EINA_TRUE);
+             ed->was_oob = EINA_FALSE;
+          }
+        if (ed->sel_unit != PUD_UNIT_NONE)
+          bitmap_cursor_state_evaluate(ed, cx, cy);
      }
 
    if (bitmap_cursor_enabled_get(ed))

bin/editor.h
@@ -119,6 +119,7 @@ struct _Editor
     * the mouse is moved within the cell */
    int prev_x;
    int prev_y;
+   Eina_Bool was_oob;
 };
 
 