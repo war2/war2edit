f4fba696b88f513aa485326f44de54319c09989c
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-08-28 14:50:31 +0200

    bitmap: fix misalignment of flying units
    Pfouuu... big bug fixed!!

--- bin/bitmap.c
+++ bin/bitmap.c
@@ -535,13 +535,15 @@ _mouse_move_cb(void        *data,
              bitmap_cursor_state_evaluate(ed, cx, cy);
              if (_resource_close_is(ed, ed->sel_unit, cx, cy))
                bitmap_cursor_enabled_set(ed, EINA_FALSE);
+
              if (pud_unit_oil_well_is(ed->sel_unit))
                {
                   /* Oil patches must be on ODD cells */
                   if ((cx % 2 == 0) || (cy % 2 == 0))
                     bitmap_cursor_enabled_set(ed, EINA_FALSE);
                }
-             else if (pud_unit_boat_is(ed->sel_unit))
+             else if (pud_unit_boat_is(ed->sel_unit) ||
+                      pud_unit_flying_is(ed->sel_unit))
                {
                   /* Boats patches must be on EVEN cells */
                   if ((cx % 2 != 0) || (cy % 2 != 0))
